9458bc08a59a400d09a1859017d7fd7649d63afb
ronoevan <ronoevan@gmail.com>
2023-04-06 17:25:25 +0200

    WEEK3, DAY4, OPTIONAL SQL GROUP BY

--- QGLgroupby.sql
+++ QGLgroupby.sql
@@ -722,5 +722,94 @@ FROM payment
 GROUP BY amount;
 #Same
 
+#WORKED
+SELECT AVG(amount)
+FROM payment;
+SELECT first_name, last_name
+FROM customer
+WHERE customer_id > (SELECT (sum(amount)/count(DISTINCT(customer_id) AS average_payment_per customer
+						FROM payment
+                        ORDER BY first_name, last_name;
+                        
+
+#############
+#WEEK3, DAY4, SQL OPTIONAL GROUP BY
+##################
+#Query 1
+#In the table actor, what last names are not repeated? For example if you would sort the data in the table actor by last_name, 
+#you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. 
+#So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", 
+#hence we would want this in our output list.
+
+USE sakila;
+
+SELECT last_name
+FROM actor
+GROUP BY last_name
+HAVING count(last_name) = 1;
+
+#OR
+
+SELECT DISTINCT last_name
+FROM actor
+GROUP BY last_name
+HAVING count(last_name) = 1;
+
+#Query 2
+#Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the 
+#last names of the actors where the last name was present more than once
+
+SELECT last_name
+FROM actor
+GROUP BY last_name
+HAVING count(last_name) > 1;
+
+#Query 3
+#Using the rental table, find out how many rentals were processed by each employee.
+
+SELECT * from rental;
+
+SELECT count(rental_id)
+FROM rental
+GROUP BY staff_id;
+
+
+#Query 4
+#Using the film table, find out how many films there are of each rating.
+
+SELECT * FROM film;
+SELECT rating, count(film_id)
+FROM film
+GROUP BY rating;
+
+
+#Query 5
+#What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
+
+SELECT * FROM film;
+
+SELECT rating, round(AVG(length), 2)
+FROM film
+GROUP BY rating;
+
+#Query 6
+#Which kind of movies (rating) have a mean duration of more than two hours?
+
+SELECT * FROM film;
+
+SELECT rating
+FROM film
+GROUP BY rating
+HAVING sum(length)/count(film_id) > 120;
+
+#OR
+
+SELECT rating
+FROM film
+GROUP BY rating
+HAVING AVG(length) > 120;
+
+
+
      
 	
